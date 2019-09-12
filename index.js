39d025ff9c366b3b944b50c553c561d599c530be
rolikasi <bayizbekovedil@gmail.com>
2019-09-12 18:56:18 +0600

    add navbar animation on scroll, footer, map, fix google chrome, padding in inputs

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function(){
+    var arr1 = ["home-navBar", "about-navBar", "team-navBar", "work-navBar", "services-navBar","features-navBar", "contact-navBar"];
+    var arr2 = ["home", "about", "team", "work", "services","features", "contact"];
+        $(document).scroll(function(){
+            for (let i = 6; i>=0; i--) {
+            if($(this).scrollTop() >= $("#" + arr2[i]).offset().top - 190){
+            $("#"+arr1[i]).addClass("ghost-button-home");
+            for (let j = i; j>=0; j--){
+             $("#"+arr1[(j-1)]).removeClass("ghost-button-home");}
+            break;
+
+
+        } if ($(this).scrollTop() < $("#"+arr2[i]).offset().top) {
+            $("#"+arr1[i]).removeClass("ghost-button-home");
+        }
+    }
+    });
+});
